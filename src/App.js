c47af2646e079eead09c687a50bd41a3cbfc38ea
nickstecker <45332665+nickstecker@users.noreply.github.com>
2021-05-16 15:34:38 +0200

    added bootrap, jQuery and first try-outs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header.js'
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-	    <h1 className="Heading"> HolidayVR </h1>
+      <Header text = "hi"/>
+      <h1 className="Heading"> HolidayVR </h1>
+      <Button variant="warning">Click me</Button>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
